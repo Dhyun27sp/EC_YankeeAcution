eacdd14f22a3bcd720ebec61d8511987156de783
Dhyun27sp <dinhtanphuchuy@gmail.com>
2022-12-23 00:21:03 +0700

    fix bug

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-const cors = require('cors')
-const app = createApp(App)
-app.use(cors)
-app.use(router)
-app.mount('#app')
--- src/service/setCookie.js
+++
@@ -1,7 +0,0 @@
-import Cookies from "js-cookie";
-
-const setCookie = (cookiename, value) => {
-    Cookies.set(cookiename, value, 1)
-};
-
-export default setCookie;
