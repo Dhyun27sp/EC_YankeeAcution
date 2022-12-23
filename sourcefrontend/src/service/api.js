f529fc5e22131a28ab9847285fd849171cea3162
dabeii <tranlan0310@gmail.com>
2022-12-23 11:32:04 +0700

    update front end

--- sourcefrontend/src/service/api.js
+++ sourcefrontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+const PORT = process.env.PORT || 5000;
 export default () => {
     return axios.create({
-        baseURL: `http://localhost:5000/`
+        baseURL: `http://localhost:${PORT}/`
     })
 }
